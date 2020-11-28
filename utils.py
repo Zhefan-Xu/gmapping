da05f343ba84d69f15bea224723543ef03bb10b7
jamie-yjh <57369446+jamie-yjh@users.noreply.github.com>
2020-11-27 19:08:11 -0500

    add sampling around the node

--- utils.py
+++ utils.py
@@ -13,13 +13,35 @@ def meas_to_pointcloud(meas, pose):
 	return pointcloud
 
 
+def sample_around(pose, delta, K, sigma_x, sigma_y, sigma_theta):
+	'''
+	pose: 3 by 1
+	delta: 3 by 1
+	'''
+	x = pose[0]
+	y = pose[1]
+	theta = pose[2]
+
+	pose_samples = np.zeros((3, K))
+	for i in range(K):
+		exit = False
+		while not exit:
+			dx = sigma_x * np.random.randn()
+			exit = abs(dx) < delta[0]
+		x_temp = x + dx
+		exit = False
+		while not exit:
+			dy = sigma_y * np.random.randn()
+			exit = abs(dy) < delta[1]
+		y_temp = y + dy
+		exit = False
+		while not exit:
+			dtheta = sigma_th * np.random.randn()
+			exit = abs(dtheta) < delta[0]
+		theta_temp = theta + dtheta
+
+		pose_samples[:, i] = np.array([x_temp, y_temp, theta_temp]).T
+
+
 if __name__ == "__main__":
-	# test SVD
-	W = np.array([[1, 2],[3, 4]])
-	WT = W.T
-	U1, _, VT1 = np.linalg.svd(W)
-	U2, _, VT2 = np.linalg.svd(WT)
-	print(U1.dot(VT1))
-	print(VT2.T.dot(U2.T))
-	print(U1,'\n', VT1)
-	print(U2,'\n', VT2)
+	print(np.array([1,2,3]).shape)
